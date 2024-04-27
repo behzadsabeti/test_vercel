56389ebecc1e9ae08da3671634fb81abb31b046a
Behzad <bsabeti13@gmail.com>
2024-04-27 15:08:28 +0330

    add main.py

--- api/index.py
+++ api/index.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI
-import uvicorn
 
 app = FastAPI()
 
@@ -7,7 +6,4 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
-
-if __name__ == '__main__':
-    uvicorn.run(app, port=8000, host='127.0.0.1')
 
